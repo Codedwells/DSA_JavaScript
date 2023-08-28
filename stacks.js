d082ba54977d4699e05f14d9e03d7f609481db34
codedwells <wells.mail.13@gmail.com>
2023-08-28 21:18:14 +0300

    New:stacks

---
+++ stacks.js
@@ -0,0 +1,44 @@
+function Stack() {
+  this.dataStore = []
+
+  this.top = 0
+  this.push = push
+  this.pop = pop
+  this.peek = peek
+  this.length = length
+  this.clear = clear
+}
+
+function peek() {
+  return this.dataStore[this.top - 1]
+}
+
+function push(element) {
+  this.dataStore[this.top++] = element
+}
+
+function pop() {
+  return this.dataStore[--this.top]
+}
+
+function peek() {
+  return this.dataStore[this.top - 1]
+}
+
+function length() {
+  return this.top
+}
+
+function clear() {
+  this.top = 0
+}
+
+let s = new Stack()
+s.push('Hello')
+s.push('world')
+
+console.log("first element is :",s.peek())
+console.log("length is :",s.length())
+
+console.log("This item was removed:",s.pop());
+console.log("This is now the top item:",s.peek());
